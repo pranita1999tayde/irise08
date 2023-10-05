7c90e54ba5b46fec6d786f7985ff70b155a6bba4
pranita1999tayde <144302372+pranita1999tayde@users.noreply.github.com>
2023-10-05 16:11:42 +0530

    update

---
+++ DAY-1-JS/day-1-js.js
@@ -0,0 +1,29 @@
+var Pranita = "9921";
+var Patil = "Akola";
+var State = "Maharashtra";
+var country = "India";
+console.log(Pranita)
+console.log(Patil)
+console.log(State)
+console.log(country)
+
+
+var I=10;
+var D=20;
+var sum = I+D;
+var sub = I-D;
+var mul = I*D;
+var div = I/D;
+console.log(sum);
+console.log(sub);
+console.log(mul);
+console.log(div);
+console.log("(-ve) value", "=", sub);
+
+
+var happy = true;
+console.log(happy);
+console.log(typeof(happy));
+
+
+
