40ba13cde42f05aa38ec0ef595fe5ab991cdf1b3
pranita1999tayde <144302372+pranita1999tayde@users.noreply.github.com>
2023-10-10 09:13:47 +0530

    update

---
+++ DAY- 3/day-3-js.js
@@ -0,0 +1,43 @@
+// var A = 25;
+// var B = 30;
+// var C = 88;
+
+// if(A % 2 == 0){
+//     console.log(`$(A) is even number.`)
+// }else {
+//     console.log(`$(A) is odd number.`)
+// }
+// if(B % 2 == 0){
+//     console.log(`$(B) is even number.`)
+// } else{
+//     console.log(`$(B) is odd number.`)
+// }
+
+// if(C % 2 == 0){
+//     console.log(`$(C) is even number.`)
+// }else{
+//     console.log(`$(C) is odd number`)
+// }
+
+
+var number = 23;
+var number2 = 78;
+var number3 = 67;
+
+if(number % 2 == 0){
+    console.log(`${number} is even`)
+}
+    if(number % 2 == 0 && number2 % 2 == 1){
+    console.log(`${number2} is odd`)
+    }else if (number % 2 == 0 &&  number2 % 2 == 0){
+      console.log(`$(number2) is even`)}
+
+else{
+    console.log(`${number} is odd`)
+ if (number % 2 == 1 && number3 % 2 == 1 ){
+    console.log(`${number3} is odd`)
+}else {console.log(`${number3} is even `)}
+}
+
+
+
