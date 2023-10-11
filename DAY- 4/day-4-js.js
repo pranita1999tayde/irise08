cb92f4d6898e6595c7acb8a0df344f5937d915eb
pranita1999tayde <144302372+pranita1999tayde@users.noreply.github.com>
2023-10-11 09:24:17 +0530

    up

---
+++ DAY- 4/day-4-js.js
@@ -0,0 +1,6 @@
+const subject=["html","css","javascript"];
+
+for(var p=0;p<subject.length;p++){
+
+    console.log(subject[p]);
+}
---
+++ DAY- 5/day-5-js.js
@@ -0,0 +1,12 @@
+function add(p,q){
+
+    console.log("add", p+q)
+    console.log("subt",p-q)
+    console.log("mult",p*q)
+    console.log("divd",p/q)
+    console.log("mod",p%q)
+
+
+}
+
+add(50,75)
